a47d212f5ffa9646c7c9a6c16de165583bb660f7
kavee <2020t00861@stu.cmb.ac.lk>
2023-01-06 23:11:58 +0530

    gitignor

--- post_ads/db.php
+++ post_ads/db.php
@@ -3,6 +3,6 @@
 $dbServername = "localhost";
 $dbUsername = "root";
 $dbPassword = "";
-$dbName = "gallerylk";
+$dbName = "register";
 
 $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
