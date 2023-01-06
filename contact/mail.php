88eda8962f91fbf1d01b7a48a609a80a7e1f68aa
chamudikaveesha <109758148+chamudikaveesha@users.noreply.github.com>
2023-01-06 12:08:01 +0530

    back

--- contact/mail.php
+++ contact/mail.php
@@ -27,7 +27,7 @@ echo'
 <body>
     <div class="container">
         <h1>Thank you for contacting us. We will get back to you as soon as possible!</h1>
-        <p class="back">Go back to the <a href="index.html">homepage</a>.</p>
+        <p class="back">Go back to the <a href="user.php">homepage</a>.</p>
         
     </div>
 </body>
