457bbeda29c4d3de75ca281137669e23f6263f43
kavee <2020t00861@stu.cmb.ac.lk>
2023-01-09 17:57:37 +0530

    fav

---
+++ Favourites/FA.php
@@ -0,0 +1,12 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','gallerylk');
+
+    $ad_id = $_POST['ad_id'];
+    $user_id = $_POST['user_id'];
+
+
+    $insert = "INSERT INTO fav(user_id,ad_id) VALUES('$user_id','$ad_id')";
+    mysqli_query($conn, $insert);
+
+?>
